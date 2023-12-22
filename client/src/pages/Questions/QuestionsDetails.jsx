c819d654ed4f60633ca2fd34e11730a282439f3e
sainathpalankar1803 <139047445+sainathpalankar1803@users.noreply.github.com>
2023-12-22 19:19:39 +0530

    Initial commit

---
+++ client/src/pages/Questions/QuestionsDetails.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import upvote from "../../assets/sort-up.svg";
+import downvote from "../../assets/sort-down.svg";
+import "./Questions.css";
+import Avatar from "../../components/Avatar/Avatar";
+import DisplayAnswer from "./DisplayAnswer";
+
+const QuestionsDetails = () => {
+  const { id } = useParams();
+  const questionsList = useSelector((state) => state.questionsReducer);
+  /* console.log(id);
+  var questionsList = [
+    {
+      _id: "1",
+      upVotes: 3,
+      downVotes: 2,
+      noOfAnswers: 2,
+      questionTitle: "What is a function?",
+      questionBody: "It means to be",
+      questionTags: [
+        "java",
+        "node js",
+        "react js",
+        "mongoodb",
+        "css",
+        "c",
+        "firebase",
+        "javascript",
+        "mysql",
+        "python",
+        "php",
+      ],
+      userPosted: "manoj",
+      userId: 1,
+      askedOn: "jan 1",
+      answer: [
+        {
+          answerBody: "Answer",
+          userAnswered: "kumar",
+          answerdOn: "jan 2",
+          userId: 2,
+        },
+      ],
+    },
+    {
+      _id: "2",
+      upVotes: 3,
+      downVotes: 2,
+      noOfAnswers: 0,
+      questionTitle: "What is a function?",
+      questionBody: "It means to be",
+      questionTags: ["javascript", "R", "python"],
+      userPosted: "manoj",
+      askedOn: "jan 1",
+      userId: 1,
+      answer: [
+        {
+          answerBody: "Answer",
+          userAnswered: "kumar",
+          answeredOn: "jan 2",
+          userId: 2,
+        },
+      ],
+    },
+    {
+      _id: "3",
+      upVotes: 3,
+      downVotes: 2,
+      noOfAnswers: 0,
+      questionTitle: "What is a function?",
+      questionBody: "It means to be",
+      questionTags: ["javascript", "R", "python"],
+      userPosted: "manoj",
+      askedOn: "jan 1",
+      userId: 1,
+      answer: [
+        {
+          answerBody: "Answer",
+          userAnswered: "kumar",
+          answerdOn: "jan 2",
+          userId: 2,
+        },
+      ],
+    },
+  ];
+*/
+  return (
+    <div className="question-details-page">
+      {questionsList.data === null ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          {questionsList.data
+            .filter((question) => question._id === id)
+            .map((question) => (
+              <div key={question._id}>
+                {console.log(question)}
+                <section className="question-details-container">
+                  <h1>{question.questionTitle}</h1>
+                  <div className="question-details-container-2">
+                    <div className="question-votes">
+                      <img
+                        src={upvote}
+                        alt=""
+                        width="17"
+                        className="votes-icon"
+                      />
+                      <p>{question.upVotes - question.downVotes}</p>
+                      <img src={downvote} alt="" width="17" />
+                    </div>
+                    <div style={{ width: "100%" }}></div>
+                    <p className="question-body">{question.questionBody}</p>
+                    <div className="question-details-tags">
+                      {question.questionTags.map((tag) => {
+                        <p key={tag}>{tag}</p>;
+                      })}
+                    </div>
+                    <div className="question-actions-user">
+                      <div>
+                        <button type="button">Share</button>
+                        <button type="button">Delete</button>
+                      </div>
+                      <div>
+                        <p>asked {question.askedOn}</p>
+                        <Link
+                          to={`/User/${question.userId}`}
+                          className="user-link"
+                          style={{ color: "#0086d8" }}
+                        >
+                          <Avatar backgroundColor="orange" px="8px" py="5px">
+                            {question.userPosted.charAt(0).toUpperCase()}
+                          </Avatar>
+
+                          <div>{question.userPosted}</div>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                {question.noOfAnswers !== 0 && (
+                  <section>
+                    <h3>{question.noOfAnswers} answers</h3>
+                    <DisplayAnswer key={question._id} question={question} />
+                  </section>
+                )}
+
+                <section className="post-ans-container">
+                  <h3>Your Answer</h3>
+                  <form>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <input
+                      type="Submit"
+                      className="post-ans-btn"
+                      value="Post Your Answer"
+                    />
+                  </form>
+                  <p>
+                    Browse other Question tagged
+                    {question.questionTags.map((tag) => (
+                      <Link to="/Tags" key={tag} className="ans-tags">
+                        {tag}
+                      </Link>
+                    ))}{" "}
+                    or
+                    <Link
+                      to="/AskQuestion"
+                      style={{ textDecoration: "none", color: "#009dff" }}
+                    >
+                      ask your own question.
+                    </Link>
+                  </p>
+                </section>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default QuestionsDetails;
